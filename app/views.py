d33c02036e4e6d27c2ab286ee8db086c5ff1e476
Zachary Tribbett <tribbettz.dev@gmail.com>
2012-10-16 10:49:19 -0400

    adding Blog Post Form; render posts on index page from followers/ees.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from forms import LoginForm, EditForm, PostForm
+from models import User, ROLE_USER, ROLE_ADMIN, Post
 from datetime import datetime
 
 @lm.user_loader
@@ -28,25 +28,22 @@ def internal_error(error):
 	return render_template('500.html'), 500
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 @login_required
 def index():
-	user = g.user
-	posts = [
-		{
-			'author': {'nickname':'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'nickname':'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(body = form.post.data, timestamp = datetime.utcnow(), author = g.user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post is now live!')
+		return redirect(url_for('index'))
+	posts = g.user.followed_posts().all()
 	return render_template("index.html",
-		title= 'Home',
-		user = user,
-		posts=posts)
+		title = 'Home',
+		form = form,
+		posts = posts)
 	
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
@@ -97,10 +94,7 @@ def user(nickname):
 	if user == None:
 		flash('User ' + nickname + ' not found.')
 		return redirect(url_for('index'))
-	posts = [
-		{ 'author': user, 'body': 'Test post 1' },
-		{ 'author': user, 'body': 'Test post 2' },
-	]
+	posts = Post.query.filter_by(author = user).all()
 	return render_template('user.html',
 		user = user,
 		posts = posts)
