293dcab230208cd235f90bd8f51ab2939a34c00b
Zachary Tribbett <tribbettz.dev@gmail.com>
2012-10-16 01:42:05 -0400

    adding webforms, validation, openid.

---
+++ config.py
@@ -0,0 +1,9 @@
+CSRF_ENABLED = True
+SECRET_KEY = '(@\xbeq\x96\x82\xda\x86t\x91\xa0\x16M\xd8a\x1c\xdf\x98\xb5*h#\xc30'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
